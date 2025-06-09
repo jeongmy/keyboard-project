6fd03d9578c5409bdb07157bfa4f66423c54584b
jeongmy <kjm9710613@naver.com>
2025-06-10 08:48:52 +0900

    nickname -> username

--- src/main/java/chosun/keyboard_project/dto/CommentResponseDTO.java
+++ src/main/java/chosun/keyboard_project/dto/CommentResponseDTO.java
@@ -8,12 +8,12 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 public class CommentResponseDTO {
-    private String nickname;
+    private String username;
     private String content;
     private LocalDateTime createdAt;
 
-    public CommentResponseDTO(String nickname, String content, LocalDateTime createdAt) {
-        this.nickname = nickname;
+    public CommentResponseDTO(String username, String content, LocalDateTime createdAt) {
+        this.username = username;
         this.content = content;
         this.createdAt = createdAt;
     }
